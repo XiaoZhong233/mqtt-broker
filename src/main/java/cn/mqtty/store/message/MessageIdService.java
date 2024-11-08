9ba655d315fc5bd79a5acb3a3636e480227cef8f
zhonghaowei <610596547@qq.com>
2024-11-08 15:45:33 +0800

    msgId使用本地锁完成

--- src/main/java/cn/mqtty/store/message/MessageIdService.java
+++ src/main/java/cn/mqtty/store/message/MessageIdService.java
@@ -8,37 +8,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by wizzer on 2018
  */
 @Component
 @Slf4j
 public class MessageIdService implements IMessageIdService {
-    @Autowired
-    private StringRedisTemplate redisService;
-
+    // 使用 AtomicInteger 来保证线程安全
+    private AtomicInteger messageId = new AtomicInteger(0);
     @Override
     public int getNextMessageId() {
         try {
-            while (true) {
-                Long increment = redisService.opsForValue().increment("mqttwk:messageid:num");
-                assert increment != null;
-                int nextMsgId = (int) (increment.intValue() % 65536);
-                if (nextMsgId > 0) {
-                    return nextMsgId;
-                }
+            // 获取当前的 messageId 并自增，使用 AtomicInteger 保证线程安全
+            int nextMsgId = messageId.incrementAndGet() % 65536;
+            if (nextMsgId == 0) {
+                // 如果值为 0，跳过，确保 ID 从 1 开始
+                nextMsgId = messageId.incrementAndGet() % 65536;
             }
+            return nextMsgId;
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
         return 0;
     }
 
-    /**
-     * 每次重启的时候初始化
-     */
-    @PostConstruct
-    public void init() {
-        redisService.delete("mqttwk:messageid:num");
-    }
+    //    @Autowired
+//    private StringRedisTemplate redisService;
+//
+//    @Override
+//    public int getNextMessageId() {
+//        try {
+//            while (true) {
+//                Long increment = redisService.opsForValue().increment("mqttwk:messageid:num");
+//                assert increment != null;
+//                int nextMsgId = (int) (increment.intValue() % 65536);
+//                if (nextMsgId > 0) {
+//                    return nextMsgId;
+//                }
+//            }
+//        } catch (Exception e) {
+//            log.error(e.getMessage(), e);
+//        }
+//        return 0;
+//    }
+//
+//    /**
+//     * 每次重启的时候初始化
+//     */
+//    @PostConstruct
+//    public void init() {
+//        redisService.delete("mqttwk:messageid:num");
+//    }
 }
