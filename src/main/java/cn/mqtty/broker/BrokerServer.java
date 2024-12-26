857976fd2263beec6555a0d0c74f00e99edf4ecf
钟浩伟 <610596547@qq.com>
2024-12-26 16:54:48 +0800

    参数优化

--- src/main/java/cn/mqtty/broker/BrokerServer.java
+++ src/main/java/cn/mqtty/broker/BrokerServer.java
@@ -115,14 +115,14 @@ public class BrokerServer {
                     }
                 })
                 .option(ChannelOption.SO_BACKLOG, brokerProperties.getSoBacklog())
-                .option(ChannelOption.SO_RCVBUF, 10 * 1024 * 1024)  // 设置接收缓冲区大小为 10MB
-                .option(ChannelOption.SO_SNDBUF, 10 * 1024 * 1024)  // 设置发送缓冲区大小为 10MB
+                .option(ChannelOption.SO_RCVBUF, 5 * 1024 * 1024)  // 设置接收缓冲区大小为 10MB
+                .option(ChannelOption.SO_SNDBUF, 5 * 1024 * 1024)  // 设置发送缓冲区大小为 10MB
                 .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
                 .option(ChannelOption.WRITE_BUFFER_WATER_MARK, new WriteBufferWaterMark(512 * 1024, 1024 * 1024))
                 .childOption(ChannelOption.SO_KEEPALIVE, brokerProperties.isSoKeepAlive())
-                .childOption(ChannelOption.SO_RCVBUF, 16 * 1024 * 1024)
-                .childOption(ChannelOption.SO_SNDBUF, 16 * 1024 * 1024)
-                .childOption(ChannelOption.TCP_NODELAY, true);
+                .childOption(ChannelOption.SO_RCVBUF, 5 * 1024 * 1024)
+                .childOption(ChannelOption.SO_SNDBUF, 5 * 1024 * 1024);
+//                .childOption(ChannelOption.TCP_NODELAY, true);
         if (Strings.isNotBlank(brokerProperties.getHost())) {
             channel = sb.bind(brokerProperties.getHost(), brokerProperties.getPort()).sync().channel();
         } else {
